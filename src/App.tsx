2084b813b0cdfa0823a299cac1c4783255471686
Alessio Bianchetti <alessio.bianchetti@boc-group.com>
2023-09-06 22:52:40 +0200

    feat: new wheel of fortune component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { useState } from "react";
 import "./App.css";
 import { OptionsList } from "./components/OptionsList";
-import WheelOfFortune from "./components/WheelOfFortune";
+import { WheelOfFortune } from "./components/WheelOfFortune";
 
 function App() {
-  const [options, setOptions] = useState<string[]>(["test1", "test2"]);
+  const [options, setOptions] = useState<string[]>(["test1", "test2", "test3", "test4", "test5"]);
+
+  const onFinished = (winner: string) => {
+    console.log(winner);
+  };
+
+  const segColors = ["#EE4040", "#F0CF50", "#815CD1", "#3DA5E0", "#34A24F", "#F9AA1F", "#EC3F3F", "#FF9000"];
 
   return (
     <div className="grid grid-cols-2 gap-2">
@@ -12,7 +18,18 @@ function App() {
         <OptionsList options={options} setOptions={setOptions}></OptionsList>
       </div>
       <div className="centered-container">
-        <WheelOfFortune options={options} />
+        <WheelOfFortune
+          segments={options}
+          segColors={segColors}
+          onFinished={(winner) => onFinished(winner)}
+          primaryColor="black"
+          contrastColor="white"
+          buttonText="Spin"
+          isOnlyOnce={false}
+          size={290}
+          upDuration={100}
+          downDuration={1000}
+        />
       </div>
     </div>
   );
--- src/components/WheelOfFortune.tsx
+++ src/components/WheelOfFortune.tsx
@@ -1,63 +1,238 @@
-import React from "react";
+import { useEffect, useState } from "react";
 
 interface WheelOfFortuneProps {
-  options: string[];
+  segments: string[];
+  segColors: string[];
+  winningSegment?: string;
+  onFinished: (winner: string) => void;
+  primaryColor?: string;
+  contrastColor?: string;
+  buttonText?: string;
+  isOnlyOnce?: boolean;
+  size?: number;
+  upDuration?: number;
+  downDuration?: number;
+  fontFamily?: string;
 }
 
-const WheelOfFortune: React.FC<WheelOfFortuneProps> = ({ options }) => {
-  const sliceAngle = 360 / options.length;
-  const radius = 100; // Adjust as needed
-  const centerX = radius + 10; // Add some padding for text
-  const centerY = radius + 10; // Add some padding for text
+export const WheelOfFortune = ({
+  segments,
+  segColors,
+  winningSegment,
+  onFinished,
+  primaryColor = "black",
+  contrastColor = "white",
+  buttonText = "Spin",
+  isOnlyOnce = true,
+  size = 290,
+  upDuration = 100,
+  downDuration = 1000,
+  fontFamily = "Arial",
+}: WheelOfFortuneProps) => {
+  let currentSegment = "";
+  let isStarted = false;
+  const [isFinished, setFinished] = useState(false);
+  let timerHandle = 0;
+  const timerDelay = segments.length;
+  let angleCurrent = 0;
+  let angleDelta = 0;
+  let canvasContext: CanvasRenderingContext2D | null = null;
+  let maxSpeed = Math.PI / segments.length || 1;
+  const upTime = segments.length * upDuration;
+  const downTime = segments.length * downDuration;
+  let spinStart = 0;
+  let frames = 0;
+  const centerX = 300;
+  const centerY = 300;
 
-  const renderSlices = () => {
-    return options.map((option, index) => {
-      const startAngle = index * sliceAngle;
-      const endAngle = startAngle + sliceAngle;
+  useEffect(() => {
+    wheelInit();
+    setTimeout(() => {
+      window.scrollTo(0, 1);
+    }, 0);
+  });
 
-      const startRad = (startAngle * Math.PI) / 180;
-      const endRad = (endAngle * Math.PI) / 180;
-
-      const x1 = centerX + radius * Math.cos(startRad);
-      const y1 = centerY + radius * Math.sin(startRad);
-      const x2 = centerX + radius * Math.cos(endRad);
-      const y2 = centerY + radius * Math.sin(endRad);
-
-      // Calculate the position for the text
-      const textX = centerX + radius * 0.75 * Math.cos((startRad + endRad) / 2);
-      const textY = centerY + radius * 0.75 * Math.sin((startRad + endRad) / 2);
-
-      return (
-        <g key={index}>
-          <path
-            d={`M ${centerX} ${centerY} L ${x1} ${y1} A ${radius} ${radius} 0 0 1 ${x2} ${y2} Z`}
-            fill={`hsl(${(index * (360 / options.length)) % 360}, 70%, 70%)`}
-          >
-            <title>{option}</title>
-          </path>
-          <text x={textX} y={textY} dominantBaseline="middle" textAnchor="middle" fill="white">
-            {option}
-          </text>
-        </g>
-      );
-    });
+  const wheelInit = () => {
+    initCanvas();
+    wheelDraw();
   };
 
-  // Calculate the position for the triangle
-  const triangleX = centerX;
-  const triangleY = centerY - radius;
+  const initCanvas = () => {
+    let canvas = document.getElementById("canvas") as HTMLCanvasElement | null;
+    // console.log(navigator);
+
+    if (navigator.userAgent.indexOf("MSIE") !== -1) {
+      canvas = document.createElement("canvas");
+      canvas.setAttribute("width", "1000");
+      canvas.setAttribute("height", "600");
+      canvas.setAttribute("id", "canvas");
+      document.getElementById("wheel")!.appendChild(canvas);
+    }
+
+    canvas!.addEventListener("click", spin, false);
+    canvasContext = canvas!.getContext("2d");
+  };
+  const spin = () => {
+    isStarted = true;
+    if (timerHandle === 0) {
+      spinStart = new Date().getTime();
+      // maxSpeed = Math.PI / ((segments.length*2) + Math.random())
+      maxSpeed = Math.PI / segments.length;
+      frames = 0;
+      timerHandle = setInterval(onTimerTick, timerDelay);
+    }
+  };
+  const onTimerTick = () => {
+    frames++;
+    draw();
+    const duration = new Date().getTime() - spinStart;
+    let progress = 0;
+    let finished = false;
+    if (duration < upTime) {
+      progress = duration / upTime;
+      angleDelta = maxSpeed * Math.sin((progress * Math.PI) / 2);
+    } else {
+      if (winningSegment) {
+        if (currentSegment === winningSegment && frames > segments.length) {
+          progress = duration / upTime;
+          angleDelta = maxSpeed * Math.sin((progress * Math.PI) / 2 + Math.PI / 2);
+          progress = 1;
+        } else {
+          progress = duration / downTime;
+          angleDelta = maxSpeed * Math.sin((progress * Math.PI) / 2 + Math.PI / 2);
+        }
+      } else {
+        progress = duration / downTime;
+        angleDelta = maxSpeed * Math.sin((progress * Math.PI) / 2 + Math.PI / 2);
+      }
+      if (progress >= 1) finished = true;
+    }
+
+    angleCurrent += angleDelta;
+    while (angleCurrent >= Math.PI * 2) angleCurrent -= Math.PI * 2;
+    if (finished) {
+      setFinished(true);
+      onFinished(currentSegment);
+      clearInterval(timerHandle);
+      timerHandle = 0;
+      angleDelta = 0;
+    }
+  };
+
+  const wheelDraw = () => {
+    clear();
+    drawWheel();
+    drawNeedle();
+  };
+
+  const draw = () => {
+    clear();
+    drawWheel();
+    drawNeedle();
+  };
+
+  const drawSegment = (key: number, lastAngle: number, angle: number) => {
+    const ctx = canvasContext!;
+    const value = segments[key];
+
+    ctx.save();
+    ctx.beginPath();
+    ctx.moveTo(centerX, centerY);
+    ctx.arc(centerX, centerY, size, lastAngle, angle, false);
+    ctx.lineTo(centerX, centerY);
+    ctx.closePath();
+    ctx.fillStyle = segColors[key];
+    ctx.fill();
+    ctx.stroke();
+    ctx.save();
+    ctx.translate(centerX, centerY);
+    ctx.rotate((lastAngle + angle) / 2);
+    ctx.fillStyle = contrastColor;
+    ctx.font = "bold 1em " + fontFamily;
+    ctx.fillText(value.substr(0, 21), size / 2 + 20, 0);
+    ctx.restore();
+  };
+
+  const drawWheel = () => {
+    const ctx = canvasContext!;
+    let lastAngle = angleCurrent;
+    const len = segments.length;
+    const PI2 = Math.PI * 2;
+
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = primaryColor;
+    ctx.textBaseline = "middle";
+    ctx.textAlign = "center";
+    ctx.font = "1em " + fontFamily;
+    for (let i = 1; i <= len; i++) {
+      const angle = PI2 * (i / len) + angleCurrent;
+      drawSegment(i - 1, lastAngle, angle);
+      lastAngle = angle;
+    }
+
+    // Draw a center circle
+    ctx.beginPath();
+    ctx.arc(centerX, centerY, 50, 0, PI2, false);
+    ctx.closePath();
+    ctx.fillStyle = primaryColor;
+    ctx.lineWidth = 10;
+    ctx.strokeStyle = contrastColor;
+    ctx.fill();
+    ctx.font = "bold 1em " + fontFamily;
+    ctx.fillStyle = contrastColor;
+    ctx.textAlign = "center";
+    ctx.fillText(buttonText, centerX, centerY + 3);
+    ctx.stroke();
+
+    // Draw outer circle
+    ctx.beginPath();
+    ctx.arc(centerX, centerY, size, 0, PI2, false);
+    ctx.closePath();
+
+    ctx.lineWidth = 10;
+    ctx.strokeStyle = primaryColor;
+    ctx.stroke();
+  };
+
+  const drawNeedle = () => {
+    const ctx = canvasContext!;
+
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = contrastColor;
+    ctx.fillStyle = contrastColor;
+    ctx.beginPath();
+    ctx.moveTo(centerX + 20, centerY - 50);
+    ctx.lineTo(centerX - 20, centerY - 50);
+    ctx.lineTo(centerX, centerY - 70);
+    ctx.closePath();
+    ctx.fill();
+    const change = angleCurrent + Math.PI / 2;
+    let i = segments.length - Math.floor((change / (Math.PI * 2)) * segments.length) - 1;
+    if (i < 0) i = i + segments.length;
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+    ctx.fillStyle = primaryColor;
+    ctx.font = "bold 1.5em " + fontFamily;
+    currentSegment = segments[i];
+    isStarted && ctx.fillText(currentSegment, centerX + 10, centerY + size + 50);
+  };
+
+  const clear = () => {
+    const ctx = canvasContext!;
+
+    ctx.clearRect(0, 0, 1000, 800);
+  };
 
   return (
-    <svg width={2 * (radius + 10)} height={2 * (radius + 10)}>
-      <g transform={`translate(${0}, ${0})`}>{renderSlices()}</g>
-      <polygon
-        points={`${triangleX},${triangleY - 10} ${triangleX - 10},${triangleY + 10} ${triangleX + 10},${
-          triangleY + 10
-        }`}
-        fill="black"
+    <div id="wheel">
+      <canvas
+        id="canvas"
+        width="1000"
+        height="800"
+        style={{
+          pointerEvents: isFinished && isOnlyOnce ? "none" : "auto",
+        }}
       />
-    </svg>
+    </div>
   );
 };
-
-export default WheelOfFortune;
